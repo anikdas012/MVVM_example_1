b3228ccb0dd4217438d9d468481b14b639677fb3
Anik Das <anik.das@pathao.com>
2019-02-19 14:22:27 +0600

    Renamed activity and layout

--- app/src/main/java/anikdas012/anikdas/tk/mvvm_try/QuotesActivity.kt
+++ app/src/main/java/anikdas012/anikdas/tk/mvvm_try/QuotesActivity.kt
@@ -7,13 +7,13 @@ import android.util.Log
 /**
  * Created by anikdas012 on 2/19/2019
  */
-class MainActivity: AppCompatActivity() {
+class QuotesActivity: AppCompatActivity() {
 
     private val LOG_TAG = "Main_Activity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(LOG_TAG, "onCreate")
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_quotes)
     }
 }
