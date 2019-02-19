dec6a741561822ceb3c8396919596faf76481282
Anik Das <anikdas012@gmail.com>
2019-02-19 18:37:00 +0600

    Created a companinon object and variable for instance.
     It is a volatile object meaning every thread will konw about this object just after creating it.

--- app/src/main/java/anikdas012/anikdas/tk/mvvm_try/data/FakeDatabase.kt
+++ app/src/main/java/anikdas012/anikdas/tk/mvvm_try/data/FakeDatabase.kt
@@ -3,5 +3,9 @@ package anikdas012.anikdas.tk.mvvm_try.data
 /**
  * Created by anikdas012 on 2/19/2019
  */
-class FakeDatabase {
+class FakeDatabase private constructor(){
+
+    companion object {
+        @Volatile private var instance: FakeDatabase? = null
+    }
 }
