3f673f6cb58fc04bd84d410261a1a5e60e328055
Anik Das <anik.das@pathao.com>
2019-02-19 12:29:55 +0600

    Deleted old java acitivity and add new kotlin activity with layout

---
+++ app/src/main/java/anikdas012/anikdas/tk/mvvm_try/MainActivity.kt
@@ -0,0 +1,9 @@
+package anikdas012.anikdas.tk.mvvm_try
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by anikdas012 on 2/19/2019
+ */
+class MainActivity: AppCompatActivity() {
+}
