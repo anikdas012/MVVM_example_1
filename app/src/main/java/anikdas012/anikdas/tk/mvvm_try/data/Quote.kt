6ceb817878b24e0c5cc8833293d57fd8d5fbb88b
Anik Das <anik.das@pathao.com>
2019-02-19 14:42:46 +0600

    Added all the necessary properties to the data class. This class will store data instance

--- app/src/main/java/anikdas012/anikdas/tk/mvvm_try/data/Quote.kt
+++ app/src/main/java/anikdas012/anikdas/tk/mvvm_try/data/Quote.kt
@@ -3,5 +3,8 @@ package anikdas012.anikdas.tk.mvvm_try.data
 /**
  * Created by anikdas012 on 2/19/2019
  */
-class Quote {
+data class Quote(val quoteText: String, val author: String) {
+    override fun toString(): String {
+        return "$quoteText - $author"
+    }
 }
