7ee40ca75fde594aca2634d022fca0b10d21d19d
Anik Das <anik.das@pathao.com>
2019-02-19 15:33:28 +0600

    Created an object of mutableList to be used as Database and a mutable Live Data object to observe the list

--- app/src/main/java/anikdas012/anikdas/tk/mvvm_try/data/FakeQuoteDAO.kt
+++ app/src/main/java/anikdas012/anikdas/tk/mvvm_try/data/FakeQuoteDAO.kt
@@ -1,7 +1,11 @@
 package anikdas012.anikdas.tk.mvvm_try.data
 
+import androidx.lifecycle.MutableLiveData
+
 /**
  * Created by anikdas012 on 2/19/2019
  */
 class FakeQuoteDAO {
+    private val quoteList = mutableListOf<Quote>()
+    private val quote = MutableLiveData<List<Quote>>()
 }
