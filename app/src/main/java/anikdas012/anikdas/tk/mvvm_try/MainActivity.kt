5d77a99975a859dcdfd290a6c49270dd0978c15e
Anik Das <anik.das@pathao.com>
2019-02-19 12:32:18 +0600

    Added onCreate() method

--- app/src/main/java/anikdas012/anikdas/tk/mvvm_try/MainActivity.kt
+++ app/src/main/java/anikdas012/anikdas/tk/mvvm_try/MainActivity.kt
@@ -1,9 +1,14 @@
 package anikdas012.anikdas.tk.mvvm_try
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 /**
  * Created by anikdas012 on 2/19/2019
  */
 class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
