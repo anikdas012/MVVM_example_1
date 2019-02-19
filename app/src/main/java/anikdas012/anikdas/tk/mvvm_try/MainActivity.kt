321d97910c2339fcb9797882857d0ad28e02c5af
Anik Das <anik.das@pathao.com>
2019-02-19 12:33:15 +0600

    Added log statements

--- app/src/main/java/anikdas012/anikdas/tk/mvvm_try/MainActivity.kt
+++ app/src/main/java/anikdas012/anikdas/tk/mvvm_try/MainActivity.kt
@@ -2,13 +2,17 @@ package anikdas012.anikdas.tk.mvvm_try
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 /**
  * Created by anikdas012 on 2/19/2019
  */
 class MainActivity: AppCompatActivity() {
 
+    private val LOG_TAG = "Main_Activity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(LOG_TAG, "onCreate")
     }
 }
