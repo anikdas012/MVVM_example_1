073667e2c50bfbb28df2d89d98c223fce967fb0a
Anik Das <anikdas012@gmail.com>
2019-02-20 10:34:49 +0600

    Added new class for Quote View Model

---
+++ app/src/main/java/anikdas012/anikdas/tk/mvvm_try/ui/quotes/QuotesViewModel.kt
@@ -0,0 +1,10 @@
+package anikdas012.anikdas.tk.mvvm_try.ui.quotes
+
+import androidx.lifecycle.ViewModel
+import anikdas012.anikdas.tk.mvvm_try.data.QuoteRepository
+
+/**
+ * Created by anikdas012 on 2/20/2019
+ */
+class QuotesViewModel(private val quoteRepository: QuoteRepository): ViewModel() {
+}
