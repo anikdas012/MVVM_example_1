97b9fe263651362c9cb76104c890b1ba6c1ebbc8
Anik Das <anikdas012@gmail.com>
2019-02-20 11:44:40 +0600

    Created a method to provide QuotesViewModelFactory()

--- app/src/main/java/anikdas012/anikdas/tk/mvvm_try/utillities/InjectorUtils.kt
+++ app/src/main/java/anikdas012/anikdas/tk/mvvm_try/utillities/InjectorUtils.kt
@@ -1,7 +1,16 @@
 package anikdas012.anikdas.tk.mvvm_try.utillities
 
+import anikdas012.anikdas.tk.mvvm_try.data.FakeDatabase
+import anikdas012.anikdas.tk.mvvm_try.data.QuoteRepository
+import anikdas012.anikdas.tk.mvvm_try.ui.quotes.QuotesViewModelFactory
+
 /**
  * Created by anikdas012 on 2/20/2019
  */
 object InjectorUtils {
+
+    fun provideQuotesViewModelFactory(): QuotesViewModelFactory {
+        val quoteRepository = QuoteRepository.getInstance(FakeDatabase.getInstance().quoteDAO)
+        return QuotesViewModelFactory(quoteRepository)
+    }
 }
