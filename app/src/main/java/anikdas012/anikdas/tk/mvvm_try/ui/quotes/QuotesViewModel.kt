bea74cce466580e242f9fe134d74a76da21c1a57
Anik Das <anikdas012@gmail.com>
2019-02-20 10:36:45 +0600

    Added method to add and get quotes from QuotesRepository

--- app/src/main/java/anikdas012/anikdas/tk/mvvm_try/ui/quotes/QuotesViewModel.kt
+++ app/src/main/java/anikdas012/anikdas/tk/mvvm_try/ui/quotes/QuotesViewModel.kt
@@ -1,10 +1,15 @@
 package anikdas012.anikdas.tk.mvvm_try.ui.quotes
 
 import androidx.lifecycle.ViewModel
+import anikdas012.anikdas.tk.mvvm_try.data.Quote
 import anikdas012.anikdas.tk.mvvm_try.data.QuoteRepository
 
 /**
  * Created by anikdas012 on 2/20/2019
  */
 class QuotesViewModel(private val quoteRepository: QuoteRepository): ViewModel() {
+
+    fun getQuotes() = quoteRepository.getQuotes()
+
+    fun addQuotes(quote: Quote) = quoteRepository.addQuote(quote)
 }
