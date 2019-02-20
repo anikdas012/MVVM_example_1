f6ed0e3f0f9b6b182f270bdf0ae1b8ddbfe59c7a
Anik Das <anikdas012@gmail.com>
2019-02-20 10:44:48 +0600

    Added method to create instance of QuotesViewModel

--- app/src/main/java/anikdas012/anikdas/tk/mvvm_try/ui/quotes/QuotesViewModelFactory.kt
+++ app/src/main/java/anikdas012/anikdas/tk/mvvm_try/ui/quotes/QuotesViewModelFactory.kt
@@ -1,5 +1,6 @@
 package anikdas012.anikdas.tk.mvvm_try.ui.quotes
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import anikdas012.anikdas.tk.mvvm_try.data.QuoteRepository
 
@@ -7,4 +8,8 @@ import anikdas012.anikdas.tk.mvvm_try.data.QuoteRepository
  * Created by anikdas012 on 2/20/2019
  */
 class QuotesViewModelFactory(private val quoteRepository: QuoteRepository): ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return QuotesViewModel(quoteRepository) as T
+    }
 }
